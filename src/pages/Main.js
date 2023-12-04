5489e5fcb379ac7b1df31fc8d6e3c63fc95cc06a
bousenkeed <ant.sakh38@gmail.com>
2023-12-04 20:39:26 +0300

    first commit

---
+++ src/pages/Main.js
@@ -0,0 +1,19 @@
+
+import './Main.css';
+
+function Main() {
+  return (
+    <div className="App">
+
+      <div className="container-title">
+      <div className="background-container">
+      </div>
+        <h1 className='title'>
+          Приветствую на сайте выпускника Синергии
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
