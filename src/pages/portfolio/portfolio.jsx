a7efbc747d7fc2b5b0cb7bc8744f05201a8259f7
bousenkeed <ant.sakh38@gmail.com>
2023-12-08 00:34:47 +0300

    first commit

--- src/pages/portfolio/portfolio.jsx
+++ src/pages/portfolio/portfolio.jsx
@@ -6,12 +6,12 @@ function Portfolio() {
         <div className={s.main_container}>
             <div className={s.site}>
 
-                <h1 className={s.text}>Мой первый сайт</h1>
+                <a className={s.ref} href="https://github.com/bousenkeed/dzsah.git"><h1 className={s.text}>Мой первый сайт</h1></a>
                 <div className={s.site_container}>
                 <img className={s.image1   + ' ' + s.images} src={require("./images/img.jpg")} alt="1st-site" />
                 </div>
 
-                <h1 className={s.text }>Сайт фотографа</h1>
+                <a className={s.ref} href="https://github.com/bousenkeed/fotolab.git"><h1 className={s.text }>Сайт фотографа</h1></a>
                 <div className={s.site_container2}>
                 <img className={s.image2    + ' ' + s.images} src={require("./images/img2.jpg")} alt="1st-site" />
                 <img className={s.image2    + ' ' + s.images} src={require("./images/img3.jpg")} alt="1st-site" />
